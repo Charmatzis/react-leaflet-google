6b48f16b060ddd46c279c66e57029180a637e92c
Christos <xr_xarmatzis@outlook.com>
2017-09-04 14:52:57 +0300

    added to examples google libraries

--- example/google.js
+++ example/google.js
@@ -23,7 +23,7 @@ export default class GoogleExample extends React.Component {
             <TileLayer  url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"/>
           </BaseLayer>
          <BaseLayer checked name='Google Maps Roads'>
-            <GoogleLayer googlekey={key}  maptype={road}/>
+            <GoogleLayer googlekey={key}  maptype={road} />
           </BaseLayer>
          <BaseLayer  name='Google Maps Terrain'>
             <GoogleLayer googlekey={key}  maptype={terrain} />
@@ -32,8 +32,11 @@ export default class GoogleExample extends React.Component {
             <GoogleLayer googlekey={key}  maptype={satellite} />
           </BaseLayer>
             <BaseLayer  name='Google Maps Hydrid'>
-            <GoogleLayer googlekey={key}  maptype={hydrid} />
-          </BaseLayer>          
+            <GoogleLayer googlekey={key}  maptype={hydrid}  libraries={['geometry', 'places']} />
+          </BaseLayer>  
+          <BaseLayer  name='Google Maps with Libraries'>
+            <GoogleLayer googlekey={key}  maptype={hydrid}  libraries={['geometry', 'places']} />
+          </BaseLayer>        
         </LayersControl>
       </Map>
     )
